aafa086313bc71f017d9165ce211047ed2a7b11e
carlosherrerah <carlos.herrera@upa.edu.mx>
2025-10-13 12:00:17 -0600

    Automatico

--- src/p2/TestAlumno.java
+++ src/p2/TestAlumno.java
@@ -1,5 +1,20 @@
 package p2;
 
+import java.time.LocalDate;
+
 public class TestAlumno {
+
+    public static void main(String[] args) {
+        Alumno[] alumnos = new Alumno[3];
+
+        alumnos[0] = new Alumno(1, "Pepito", LocalDate.of(2025,10,10), null);
+        alumnos[1] = new Alumno(10, "Mafalda", LocalDate.parse("2000-01-01"), new Cuenta(55, "Mafalda", 500));
+
+        for (int i = 0; i < alumnos.length; i++) {
+            System.out.println("Nombre: " + alumnos[i].nombre +
+                    "  Fecha Nac: " + alumnos[i].getFechaNacimiento() +
+                    "  Dia: " + alumnos[i].diaSemana());
+        }
+    }
     
 }
